26081f75183ffa1073a33d05eb16c7592b8ac9ba
spencer-mcguire <smcguire1826@gmail.com>
2020-01-13 14:46:10 -0600

    GET user by id

--- index.js
+++ index.js
@@ -46,6 +46,29 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+//GET users by ID
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
+    });
+});
+
+//DELETE a user
+// server.delete();
+
 server.listen(port, () => {
   console.log(`\n Server lisening on Port:${port}`);
 });
