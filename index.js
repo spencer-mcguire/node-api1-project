e2cbe39e96e0813808cd364d97822634972aa78a
spencer-mcguire <smcguire1826@gmail.com>
2020-01-13 14:36:07 -0600

    GET AND POST REQUESTS

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // implement your API here
+const express = require("express");
+
+const Users = require("./data/db.js");
+
+const server = express();
+server.use(express.json());
+
+const port = 8000;
+
+//REQUEST
+
+//POST new user
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
+
+// GET all users
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
+    });
+});
+
+server.listen(port, () => {
+  console.log(`\n Server lisening on Port:${port}`);
+});
